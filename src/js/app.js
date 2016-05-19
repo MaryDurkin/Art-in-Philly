5fbba70b6f8313c70247f93d0046559160bb47d5
Mary <mary.walsh@gmail.com>
2016-05-19 19:56:41 -0400

    Finally able to toggle markers - some debug code left in here

--- src/js/app.js
+++ src/js/app.js
@@ -34,19 +34,64 @@ var artPlaces =  [
 var map;
 var service;
 var infowindow;
+var locationMarkers = [];
+var marker;
+var i;
+var PHILLY = {lat: 39.9526, lng: -75.1652};
+
+function initMap() {
+    //var philly = new google.maps.LatLng(39.9526, -75.1652);
+
+    map = new google.maps.Map(document.getElementById('map'), {
+    	center: PHILLY, //{lat: 39.9526, lng: -75.1652},			// Philadelphia PA
+    	zoom: 14
+    });
+
+  	//create infoWindow
+  	infoWindow = new google.maps.InfoWindow({
+	   });
+
+
+
+  	//This code works to set markers...
+
+  	for (i = 0; i < artPlaces.length; i++) {
+
+			marker = new google.maps.Marker({
+	      			map: map,
+	      			position: artPlaces[i].position,
+	      			label: artPlaces[i].type
+	      		});
+
+
+			marker.addListener('click', (function(markerCopy, iCopy) {
+				return function(){
+
+							infoWindow.setContent(artPlaces[iCopy].name);
+		      		 		infoWindow.open(map, markerCopy);
+
+		      			};
+		    })(marker, i));
+			locationMarkers[i] = marker;
+			if (i===3) {
+				locationMarkers[2].setVisible(false);
+			}
+	}
+	console.log(locationMarkers);
+	locationMarkers[4].setVisible(false);
+}
 
 var ArtLocation = function(data){
-	/*
-	this.name = ko.observable(data.name);
-	this.position = ko.observable(data.position);
-	this.type = ko.observable(data.type);
-	*/
+
 	this.name = data.name;
 	this.position = data.position;
 	this.type = data.type;
+	this.marker = data.marker;
 };
 var ViewModel = function() {
 	var self = this;
+
+	self.googleMap = map;
 	this.location = ko.observable("Haverford");
 
 	this.artList = ko.observableArray([]);
@@ -56,36 +101,26 @@ var ViewModel = function() {
 		self.artList.push(new ArtLocation(artPlaceItem));
 	});
 
-	//console.log(map);
 
-	console.log(this.artList().length);
-	console.log(this.artList()[0].position);
-	console.log(this.artList()[0].type);
-	console.log(this.artList()[0].name);
-
-	/* this.currentCat = ko.observable(this.catList()[0]);
-	this.setCurrentCat = function(nextCat) {
-
-		self.currentCat(nextCat);
-
-	}; */
 
 	this.currentAttraction = ko.observable(this.artList()[0]);
 	console.log("Here");
 	console.log(this.currentAttraction().name);
+	console.log(locationMarkers);
 	// Update current attraction
 	this.setCurrentAttraction = function(nextAttraction){
-		console.log("Which Damm Attraction!");
+
 		self.currentAttraction(nextAttraction);
 		//reset the window content and position
+		console.log(self.currentAttraction().name);
+		console.log(locationMarkers);
+		console.log(locationMarkers[0]);
+		locationMarkers[0].setVisible(false);
 		infoWindow.setContent(self.currentAttraction().name);
 		infoWindow.setPosition(self.currentAttraction().position);
 	};
 
 	this.moveWindow = function(name) {
-
-		console.log("this is where I figure out where to put the window");
-		//infoWindow.setContent(self.currentAttraction().name);
 
 		console.log(self.currentAttraction().name);
 		infoWindow.setPosition(self.currentAttraction().position);
@@ -97,48 +132,6 @@ var ViewModel = function() {
 
 
 
-function initMap() {
-    //var philly = new google.maps.LatLng(39.9526, -75.1652);
-    var philly = {lat: 39.9526, lng: -75.1652};
-    	map = new google.maps.Map(document.getElementById('map'), {
-    		center: philly, //{lat: 39.9526, lng: -75.1652},			// Philadelphia PA
-    		zoom: 14
-    	});
-
-    /*
-    var request = {
-    		location: philly,
-    		radius: '2500',
-    		types: ['museum','art_gallery','store']
-  		};
-	*/
-  	//service = new google.maps.places.PlacesService(map);
-  	//service.nearbySearch(request, callback);
-
-  	//create infoWindow
-  	infoWindow = new google.maps.InfoWindow({
-	   });
-
-  	//This code works to set markers...
-
-  	for (var i = 0; i < artPlaces.length; i++) {
-
-			var marker = new google.maps.Marker({
-	      			map: map,
-	      			position: artPlaces[i].position,
-	      			label: artPlaces[i].type
-	      		});
-
-			marker.addListener('click', (function(markerCopy, iCopy) {
-				return function(){
-
-							infoWindow.setContent(artPlaces[iCopy].name);
-		      		 		infoWindow.open(map, markerCopy);
-		      			};
-		    })(marker, i));
-	}
-
-}
 
 ko.applyBindings(new ViewModel());
 
