f5122b56bdb318a4130a28c036de15775dacfa33
Mary <mary.walsh@gmail.com>
2016-05-20 08:46:32 -0400

    neatened up code before adding filter

--- src/js/app.js
+++ src/js/app.js
@@ -32,68 +32,57 @@ var artPlaces =  [
         ];
 
 var map;
-var service;
 var infowindow;
 var locationMarkers = [];
 var marker;
-var i;
-var PHILLY = {lat: 39.9526, lng: -75.1652};
 
 function initMap() {
-    //var philly = new google.maps.LatLng(39.9526, -75.1652);
 
+    // create map
+    var PHILLY = {lat: 39.9526, lng: -75.1652};
     map = new google.maps.Map(document.getElementById('map'), {
     	center: PHILLY, //{lat: 39.9526, lng: -75.1652},			// Philadelphia PA
     	zoom: 14
     });
 
-  	//create infoWindow
+  	//create infoWindow this window will have it's content and location assigned/changed when appropriate
   	infoWindow = new google.maps.InfoWindow({
 	   });
-
-
-
-  	//This code works to set markers...
-
-  	for (i = 0; i < artPlaces.length; i++) {
+  	// set markers...
+  	for (var i = 0; i < artPlaces.length; i++) {
 
 			marker = new google.maps.Marker({
 	      			map: map,
 	      			position: artPlaces[i].position,
 	      			label: artPlaces[i].type
 	      		});
-
-
+			//when marker is clicked, set window contents and open window
 			marker.addListener('click', (function(markerCopy, iCopy) {
 				return function(){
-
 							infoWindow.setContent(artPlaces[iCopy].name);
 		      		 		infoWindow.open(map, markerCopy);
-
 		      			};
 		    })(marker, i));
+		    // add marker to array of loaction markers
 			locationMarkers[i] = marker;
-			if (i===3) {
-				locationMarkers[2].setVisible(false);
-			}
+			artPlaces[i].marker = marker;
+
 	}
+	// code to see what is going on
 	console.log(locationMarkers);
-	locationMarkers[4].setVisible(false);
+	console.log(artPlaces);
 }
 
-var ArtLocation = function(data){
 
-	this.name = data.name;
-	this.position = data.position;
-	this.type = data.type;
-	this.marker = data.marker;
-};
 var ViewModel = function() {
 	var self = this;
+	var ArtLocation = function(data){
 
-	self.googleMap = map;
-	this.location = ko.observable("Haverford");
-
+		this.name = data.name;
+		this.position = data.position;
+		this.type = data.type;
+		this.marker = data.marker;
+	};
 	this.artList = ko.observableArray([]);
 
 	artPlaces.forEach(function(artPlaceItem){
@@ -102,36 +91,30 @@ var ViewModel = function() {
 	});
 
 
-
 	this.currentAttraction = ko.observable(this.artList()[0]);
 	console.log("Here");
-	console.log(this.currentAttraction().name);
+	//console.log(this.currentAttraction().name);
 	console.log(locationMarkers);
+
 	// Update current attraction
 	this.setCurrentAttraction = function(nextAttraction){
 
 		self.currentAttraction(nextAttraction);
 		//reset the window content and position
-		console.log(self.currentAttraction().name);
-		console.log(locationMarkers);
-		console.log(locationMarkers[0]);
-		locationMarkers[0].setVisible(false);
 		infoWindow.setContent(self.currentAttraction().name);
 		infoWindow.setPosition(self.currentAttraction().position);
+		infoWindow.open(map);
 	};
 
+	/*  // unused code
 	this.moveWindow = function(name) {
 
 		console.log(self.currentAttraction().name);
 		infoWindow.setPosition(self.currentAttraction().position);
 		//infoWindow.open(map, markerCopy);
 	};
-
-
+	*/
 }
-
-
-
 
 ko.applyBindings(new ViewModel());
 
