364c5ed05bd917edfe180d0db140b99567457bff
Mary <mary.walsh@gmail.com>
2016-06-01 10:55:03 -0400

    tidyed up before correcting the filter function

--- README.md
+++ README.md
@@ -4,17 +4,6 @@
 
 There are src and dist folders containing source and production code. The dist folder can be found at the gh-pages version of the repository, [here](http://marydurkin.github.io/Art-in-Philly/)
 
-Hi Heidi!
-
-I have the basic functionality completed (I haven't started the styling yet, so it looks pretty ugly - my apologies). I do have a couple of bugs that I can't figure out.
-<li> Occasionally, on refresh, I get the following error message</li>
-Uncaught InvalidValueError: initMap is not a function
-<li>The drop down menu filter options need to be clicked twice before they work (on mac Chrome) It seems to work fine on firefox nd safari. I did try the forums for this but the mentor couldn't recreate the issue (I think he was using Chrome on windows). [here is the forum link](https://discussions.udacity.com/t/using-data-bind-click-with-select-tab/169375)</li>
-
-I'm also having a problem with getting my gruntfile to correctly minify the mhtl - if there is time I would appreciate help with that too.
-
-Suggestions or work-arounds for the above appreciated.
-
 
 (If you would like to rebuild the dist files, download the repository and run the grunt command in the terminal).
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,8 @@
 /*
-There are three basic data structures that the app uses:
+There are two basic data structures that the app uses:
 	the initial one provided - artPlaces
-	an array of map markers for each of these places - locationMarkers[]
-	a KO array created from artPlaces
-	there is also currentIndex variable that is used to make sure the correct marker is referenced
+
+	a KO array created from artPlaces, with map markers added
 */
 var artPlaces =  [
         {
@@ -44,20 +43,8 @@ var artPlaces =  [
         ];
 
 var map;
-var infoWindow;
-var locationMarkers = [];
-var marker;
 // used for the data bind menu
 var placeTypes = ['All','Gallery','Museum','Supply Store'];
-
-// constructor for element of KO array artList
-var ArtLocation = function(data){
-	this.selected = ko.observable(true);
-	this.name = data.name;
-	this.position = data.position;
-	this.type = data.type;
-
-};
 
 function initMap() {
 
@@ -68,81 +55,85 @@ function initMap() {
     	zoom: 14
     });
 
-  	//create infoWindow this window will have it's content and location assigned/changed when appropriate
-  	infoWindow = new google.maps.InfoWindow({
-	   });
-  	// create markers...
-  	for (var i = 0; i < artPlaces.length; i++) {
-			marker = new google.maps.Marker({
-	      			map: map,
-	      			position: artPlaces[i].position,
-	      			label: artPlaces[i].type
-	      		});
-		    // add marker to array of loaction markers
-			locationMarkers[i] = marker;
-	}
 	ko.applyBindings(new ViewModel());
 }
 
 var ViewModel = function() {
 
 	var self = this;
-	chosenType =  ko.observable("All");
-	var currentMarker;
-	var currentIndex = 0;
-	//var lastIndex = 0;
-	this.artList = ko.observableArray([]);
+	var infoWindow;
+	var currentAttraction;
+	var marker;
+	// constructor for element of KO array artList
+	var ArtLocation = function(data){
+		this.selected = ko.observable(true);
+		this.name = data.name;
+		this.position = data.position;
+		this.type = data.type;
+		this.marker = data.marker;
 
+	};
+
+	chosenType =  ko.observable("All");
+
+	// An observable array to store locations, their data and markers
+	artList = ko.observableArray([]);
+
+	//populate KO list of places
+	artPlaces.forEach(function(artPlaceItem){
+		artList.push(new ArtLocation(artPlaceItem));
+	});
+
+	//create infoWindow, this window will have it's content and location assigned/changed when appropriate
+  	infoWindow = new google.maps.InfoWindow({
+	   });
+
+  	//create markers, add them to the artList and assign listeners
+	for (var i = 0; i < artList().length; i++) {
+			marker = new google.maps.Marker({
+	      			map: map,
+	      			position: artList()[i].position,
+	      			label: artList()[i].type
+	      		});
+
+			artList()[i].marker = marker;
+
+			marker.addListener('click', (function(markerCopy, iCopy) {
+				return function(){
+					setMarkerBounce(markerCopy);
+					setWindow(artList()[iCopy]);
+		    	};
+			})(marker, i));
+	};
+
+
+	// initial value
+	currentAttraction = artList()[0];
 	// setWindow gets the info from foursquare and updates the content of the infowindow
-	var setWindow = function(place,index) {
+	var setWindow = function(attraction) {
 
 		var CLIENT_ID = "NB2NRF1KT2Q2ZXV2XLNWX5OFP2MCBZN4ZSUHGCTA2UTEPX0I";
 		var CLIENT_SECRET = "WZDS21MLCN3ETHUVXOZSZPFA13T2LJFEU2P0WWQYXCBCA2KI";
-		var FourSquareURL = "https://api.foursquare.com/v2/venues/search?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=20130815&ll=39.9526,-75.1652&query=" + place + "&match&limit=1";
+		var FourSquareURL = "https://api.foursquare.com/v2/venues/search?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=20130815&ll=39.9526,-75.1652&query=" + attraction.name + "&match&limit=1";
 
 			$.getJSON(FourSquareURL, function(data) {
 
 				if (data.response.venues.length >0) {
-					infoWindow.setContent(place + " "+data.response.venues[0].location.address + " " +data.response.venues[0].contact.formattedPhone);
-					infoWindow.open(map,locationMarkers[index]);
+					infoWindow.setContent(attraction.name + " "+data.response.venues[0].location.address + " " +data.response.venues[0].contact.formattedPhone);
+					infoWindow.open(map,attraction.marker);
 				}
 				//This is used in the event of the .getJSON working but foursquare not having information on that location
 				else {
-					infoWindow.setContent(place + " further information not available");
-					infoWindow.open(map,locationMarkers[index]);
+					infoWindow.setContent(attraction.name + " further information not available");
+					infoWindow.open(map,attraction.marker);
 				}
 
 			}).fail(function(e){ // regular error handling
-				infoWindow.setContent(place + "  further information not available");
-				infoWindow.open(map,locationMarkers[index]);
+				infoWindow.setContent(attraction + "  further information not available");
+				infoWindow.open(map,attraction.marker);
 			});
 
 	};
-
-	//populate KO list of places
-	artPlaces.forEach(function(artPlaceItem){
-
-		self.artList.push(new ArtLocation(artPlaceItem));
-	});
-
-
-
-	// add event listeners to the markers
-	for (var i = 0; i < locationMarkers.length; i++) {
-		currentMarker = locationMarkers[i];
-
-		currentMarker.addListener('click', (function(currentMarkerCopy, iCopy) {
-
-			return function(){
-				setMarkerBounce(currentMarkerCopy);
-				setWindow(artPlaces[iCopy].name,iCopy);
-
-		    };
-		})(currentMarker, i));
-	}
-
-	// initial value
-	this.currentAttraction = ko.observable(this.artList()[0]);
 
 	// updateVisibles is bound to the drop down menu and toggles the visibility of each
 	// item based on the menu selection
@@ -154,52 +145,37 @@ var ViewModel = function() {
 		infoWindow.close();
 		if (type === 'All') {
 			for (var i = 0; i < artPlaces.length; i++) {
-				self.artList()[i].selected(true);
-				locationMarkers[i].setVisible(true);
+				artList()[i].selected(true);
+				artList()[i].marker.setVisible(true);
 			}
 		}
 		else {
 			for (var i = 0; i< artPlaces.length; i++){
-				 if (type === self.artList()[i].type) {
-				 	self.artList()[i].selected(true);
-				 	locationMarkers[i].setVisible(true);
+				 if (type === artList()[i].type) {
+				 	artList()[i].selected(true);
+				 	artList()[i].marker.setVisible(true);
 				 }
 				 else {
-				 	self.artList()[i].selected(false);
-				 	locationMarkers[i].setVisible(false);
+				 	artList()[i].selected(false);
+				 	artList()[i].marker.setVisible(false);
 				 }
 			}
 		}
 	};
 
-	function findIndex(name) { //returns array index of currentAttraction
-
-		for (var i=0; i < artPlaces.length; i++ ) {
-			if (name === artPlaces[i].name) {
-				return i;
-			}
-		}
-		return -1;
-	};
 
 	function setMarkerBounce(marker) {
-
         marker.setAnimation(google.maps.Animation.BOUNCE);
           setTimeout(function () {
         marker.setAnimation(null);
     }, 2000);
     };
 
-
-
 	// bound to listed attractions
 	this.setCurrentAttraction = function(nextAttraction){
-		//lastIndex = currentIndex;
-		self.currentAttraction(nextAttraction);
-
-		currentIndex = findIndex(self.currentAttraction().name)
-		setMarkerBounce(locationMarkers[currentIndex]);
-		setWindow(self.currentAttraction().name, currentIndex);
+		currentAttraction = nextAttraction;
+		setMarkerBounce(currentAttraction.marker);
+		setWindow(currentAttraction);
 	};
 
 };
