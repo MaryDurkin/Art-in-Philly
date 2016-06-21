123b311768d8c06d80aeace2bb477da0a0c95530
Mary <mary.walsh@gmail.com>
2016-06-21 14:16:38 -0400

    All requirements and suggestions implemented

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 ### Art in Philly
 
-There are src and dist folders containing source and production code. The dist folder can be found at the gh-pages version of the repository, [here](http://marydurkin.github.io/Art-in-Philly/)
+There are src and dist folders containing source and production code. To run the app do one of the following:
+
+* Download the repository and run either of dist/index.html or src/index.html on a local server.
+* Head over to the gh-pages version of the repository, [here](http://marydurkin.github.io/Art-in-Philly/)
+
+(If you would like to rebuild the dist files, download the repository and run the grunt command in the terminal).
 
 The app uses the a map provided by Google and customized using the [google maps styles wizard](http://googlemaps.github.io/js-samples/styledmaps/wizard/index.html). Other content provided by [FourSquare](https://developer.foursquare.com/).
 
--- src/js/app.js
+++ src/js/app.js
@@ -4,37 +4,37 @@ There are two basic data structures that the app uses:
 	a KO array created from artPlaces, with map markers added
 */
 var artPlaces =  [
-        {
-            name: "Philadelphia Museum of Art",
-            position: {lat: 39.9658015, lng: -75.1811616},
-            type: 'Museum'
-        },
-        {
-            name: "The Barnes Foundation",
-            position: {lat: 39.9605194, lng: -75.1725901},
-            type: 'Museum'
-        },
-        {
-            name: "Rodin Museum",
-            position: {lat: 39.9619653, lng: -75.1739912},
-            type: 'Museum'
-        },
-        {
-   			name: "Pennsylvania Academy of The Fine Arts",
-            position: {lat: 39.9556041, lng: -75.1632326},
-            type: 'Museum'
-        },
-        {
-            name: "Blick Art Materials",
-            position: {lat: 39.9506031, lng: -75.1631014},
-            type: 'Supply Store'
-        },
-        {
-            name: "Philadelphia's Magic Gardens",
-            position: {lat: 39.942642, lng: -75.1592851},
-            type: 'Gallery'
-        }
-        ];
+		{
+        	name: "Philadelphia Museum of Art",
+			position: {lat: 39.9658015, lng: -75.1811616},
+			type: 'Museum'
+		},
+		{
+			name: "The Barnes Foundation",
+			position: {lat: 39.9605194, lng: -75.1725901},
+			type: 'Museum'
+		},
+		{
+			name: "Rodin Museum",
+			position: {lat: 39.9619653, lng: -75.1739912},
+			type: 'Museum'
+		},
+		{
+			name: "Pennsylvania Academy of The Fine Arts",
+			position: {lat: 39.9556041, lng: -75.1632326},
+			type: 'Museum'
+		},
+		{
+			name: "Blick Art Materials",
+			position: {lat: 39.9506031, lng: -75.1631014},
+			type: 'Supply Store'
+		},
+		{
+			name: "Philadelphia's Magic Gardens",
+			position: {lat: 39.942642, lng: -75.1592851},
+			type: 'Gallery'
+		}
+		];
 
 var map;
 // used for the data bind menu and possible later in the apps future for a 'select by type' menu option
@@ -45,63 +45,62 @@ function initMap() {
 // This JSON generated using the Google Maps style wizard:
 // http://googlemaps.github.io/js-samples/styledmaps/wizard/index.html
 	var styles = [
-	  {
-	    "featureType": "water",
-	    "stylers": [
-	      { "color": "#92a8d1" }
-	    ]
-	  },{
-	    "featureType": "poi.park",
-	    "stylers": [
-	      { "lightness": 29 },
-	      { "saturation": -12 },
-	      { "hue": "#00ff77" }
-	    ]
-	  },{
-	    "featureType": "landscape.man_made",
-	    "stylers": [
-	      { "hue": "#ffbb00" },
-	      { "saturation": 17 },
-	      { "lightness": -8 }
-	    ]
-	  },{
-	    "featureType": "transit.line",
-	    "stylers": [
-	      { "color": "#9896a4" }
-	    ]
-	  },{
-	    "featureType": "transit.station",
-	    "stylers": [
-	      { "color": "#9896a4" }
-	    ]
-	  },{
-	    "featureType": "poi.school",
-	    "stylers": [
-	      { "color": "#b18f6a" }
-	    ]
-	  },{
-	    "featureType": "road.arterial",
-	    "stylers": [
-	      { "saturation": -22 }
-	    ]
-	  }
+		{
+			"featureType": "water",
+			"stylers": [
+				{ "color": "#92a8d1" }
+			]
+		},{
+			"featureType": "poi.park",
+			"stylers": [
+				{ "lightness": 29 },
+				{ "saturation": -12 },
+				{ "hue": "#00ff77" }
+			]
+		},{
+			"featureType": "landscape.man_made",
+			"stylers": [
+				{ "hue": "#ffbb00" },
+				{ "saturation": 17 },
+				{ "lightness": -8 }
+			]
+		},{
+			"featureType": "transit.line",
+			"stylers": [
+				{ "color": "#9896a4" }
+			]
+		},{
+			"featureType": "transit.station",
+			"stylers": [
+				{ "color": "#9896a4" }
+			]
+		},{
+			"featureType": "poi.school",
+			"stylers": [
+				{ "color": "#b18f6a" }
+			]
+		},{
+			"featureType": "road.arterial",
+			"stylers": [
+				{ "saturation": -22 }
+			]
+		}
 	];
 
 	var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"});
-    // create map
-    var PHILLY = {lat: 39.9526, lng: -75.1652};
-    var mapOptions = {
-    	center: PHILLY,
-    	zoom: 14,
-    	mapTypeControl: false,
-    	mapTypeControlOptions: {
-    		mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
-    	}
-    };
-   	map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    map.mapTypes.set('map_style', styledMap);
-  	map.setMapTypeId('map_style');
-
+	// create map
+	var PHILLY = {lat: 39.9526, lng: -75.1652};
+	var mapOptions = {
+		center: PHILLY,
+		zoom: 14,
+		mapTypeControl: false,
+		mapTypeControlOptions: {
+		mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+		}
+	};
+	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	map.mapTypes.set('map_style', styledMap);
+	map.setMapTypeId('map_style');
 
 	ko.applyBindings(new ViewModel());
 }
@@ -113,8 +112,7 @@ var ViewModel = function() {
 	var currentAttraction;
 	var marker;
 	var text;
-	query =  "";
-
+	var query;
 	// constructor for element of KO array artList
 	var ArtLocation = function(data){
 		this.selected = ko.observable(true);
@@ -122,7 +120,6 @@ var ViewModel = function() {
 		this.position = data.position;
 		this.type = data.type;
 		this.marker = data.marker;
-
 	};
 
 	// An observable array to store locations, their data and markers
@@ -134,16 +131,16 @@ var ViewModel = function() {
 	});
 
 	//create infoWindow, this window will have it's content and location assigned/changed when appropriate
-  	infoWindow = new google.maps.InfoWindow({
-	   });
+	infoWindow = new google.maps.InfoWindow({
+		});
 
   	//create markers, add them to the artList and assign listeners
 	for (var i = 0; i < artList().length; i++) {
 			marker = new google.maps.Marker({
-	      			map: map,
-	      			position: artList()[i].position,
-	      			label: artList()[i].type
-	      		});
+					map: map,
+					position: artList()[i].position,
+					label: artList()[i].type
+				});
 
 			artList()[i].marker = marker;
 
@@ -152,58 +149,60 @@ var ViewModel = function() {
 					currentAttraction = artList()[iCopy];
 					setMarkerBounce(markerCopy);
 					setWindow(currentAttraction);
-		    	};
+				};
 			})(marker, i));
 	}
 
-
 	// setWindow gets the info from foursquare and updates the content of the infowindow
-		var CLIENT_ID = "NB2NRF1KT2Q2ZXV2XLNWX5OFP2MCBZN4ZSUHGCTA2UTEPX0I";
-		var CLIENT_SECRET = "WZDS21MLCN3ETHUVXOZSZPFA13T2LJFEU2P0WWQYXCBCA2KI";
-		var contentString;
+	var CLIENT_ID = "NB2NRF1KT2Q2ZXV2XLNWX5OFP2MCBZN4ZSUHGCTA2UTEPX0I";
+	var CLIENT_SECRET = "WZDS21MLCN3ETHUVXOZSZPFA13T2LJFEU2P0WWQYXCBCA2KI";
+	var contentString;
 
 	var setWindow = function(attraction) {
 
-			var FourSquareURL = "https://api.foursquare.com/v2/venues/search?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=20130815&ll=39.9526,-75.1652&query=" + attraction.name + "&match&limit=1";
-			var attributionURL = "http://foursquare.com/v/";
-			$.getJSON(FourSquareURL, function(data) {})
-				.done(function(data){
-				if (data.response.venues.length >0) {
-					//if the venue is found, populate the info window
-					attributionURL = "http://foursquare.com/v/"+data.response.venues[0].id;
-					contentString = '<div id="window-content">'+
-									'<h4 id="name">'+attraction.name+'</h4>'+
-									'<h5 id="address">'+data.response.venues[0].location.address+'</h5>'+
-									'<h5 id="phone">'+data.response.venues[0].contact.formattedPhone+'</h5>'+
-									'<h6 id="attribution">(Information provided by <a href="" id="four-square-return" target="_blank">FourSquare</a> )</h6>'+
-									//'<a href="" id="four-square-return" target="_blank">Read More</a>'+
-									'</div>';
-
-					infoWindow.setContent(contentString);
-					infoWindow.open(map,attraction.marker);
-					document.getElementById("four-square-return").href = attributionURL;
-
-				}
-
-				else {
-					// If the venue is not found, but the getJSON doesn't fail - populate teh info window with error message
-					contentString = '<div id="window-content">'+
-									'<h4 id="name">'+attraction.name+'</h4>'+
-									'<h6 id="error-msg">Further information not available</h6>'+
-									'</div>';
-					infoWindow.setContent(contentString);
-					infoWindow.open(map,attraction.marker);
-				}
-
-				})
-				.fail(function(e){ // regular error handling
+		var FourSquareURL = "https://api.foursquare.com/v2/venues/search?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=20130815&ll=39.9526,-75.1652&query=" + attraction.name + "&match&limit=1";
+		var attributionURL = "http://foursquare.com/v/";
+		var address;
+		var phone;
+		$.getJSON(FourSquareURL, function(data) {})
+			.done(function(data){
+			if (data.response.venues.length >0) {
+				//if the venue is found, populate the info window
+				// check phone # is returned
+				phone = data.response.venues[0].contact.formattedPhone ? data.response.venues[0].contact.formattedPhone : "No phone";
+				//check location address is returned
+				address = data.response.venues[0].location.address ? data.response.venues[0].location.address : "No address found";
+				attributionURL = "http://foursquare.com/v/"+data.response.venues[0].id;
 				contentString = '<div id="window-content">'+
 								'<h4 id="name">'+attraction.name+'</h4>'+
-								'<h6 id="attribution">Further information not available</h6>'+
+								'<h5 id="address">'+address+'</h5>'+
+								'<h5 id="phone">'+phone+'</h5>'+
+								'<h6 id="attribution">(Information provided by <a href="" id="four-square-return" target="_blank">FourSquare</a> )</h6>'+
 								'</div>';
 				infoWindow.setContent(contentString);
 				infoWindow.open(map,attraction.marker);
-				});
+				document.getElementById("four-square-return").href = attributionURL;
+				}
+
+			else {
+				// If the venue is not found, but the getJSON doesn't fail - populate the info window with error message
+				contentString = '<div id="window-content">'+
+								'<h4 id="name">'+attraction.name+'</h4>'+
+								'<h6 id="error-msg">Further information not available</h6>'+
+								'</div>';
+				infoWindow.setContent(contentString);
+				infoWindow.open(map,attraction.marker);
+			}
+
+			})
+			.fail(function(e){ // regular error handling
+			contentString = '<div id="window-content">'+
+							'<h4 id="name">'+attraction.name+'</h4>'+
+							'<h6 id="attribution">Further information not available</h6>'+
+							'</div>';
+			infoWindow.setContent(contentString);
+			infoWindow.open(map,attraction.marker);
+			});
 
 	};
 
@@ -211,9 +210,8 @@ var ViewModel = function() {
 	// location if it matches the search criteria
 	this.filter = function() {
 
-
 		infoWindow.close();
-		var lowerCaseQuery = query.toLowerCase();
+		var lowerCaseQuery = this.query.toLowerCase();
 
 		for (var i=0; i < artList().length; i++) {
 			if (artList()[i].name.toLowerCase().indexOf(lowerCaseQuery) !== -1) {
@@ -228,11 +226,11 @@ var ViewModel = function() {
 	};
 
 	function setMarkerBounce(marker) {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-          setTimeout(function () {
-        	marker.setAnimation(null);
-    	}, 2000);
-    }
+		marker.setAnimation(google.maps.Animation.BOUNCE);
+			setTimeout(function () {
+			marker.setAnimation(null);
+			}, 2000);
+	};
 
 	// bound to listed attractions/places/venues
 	this.setCurrentAttraction = function(nextAttraction){
